86762aba19611da5a2ad0fe82ebefc36f470a6a0
Shuhan <daisy_xsh@163.com>
2018-04-20 14:43:52 -0400

    near neighbor

--- js/functions.js
+++ js/functions.js
@@ -5,3 +5,15 @@ welcomebutton.click(function(){
   $('#welcome_page').hide();
 }
 );
+
+
+// Plot Nearest Station using leafletKnn
+var plotNearest = function(feature){
+  nearest = leafletKnn(feature).nearest(L.latLng($('#lat').val(),$('#lon').val()), 1);
+  nearestmarkers = [];
+  _.each(nearest,function(obj){
+    nearestmarkers.push(L.marker([obj.lat,obj.lon],{icon: myIcon}));
+  });
+  addNear=_.each(nearestmarkers,function(markers){markers.addTo(map);});
+  return addNear;
+};
